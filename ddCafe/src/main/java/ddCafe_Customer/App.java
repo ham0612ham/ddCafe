08bbd645143cc3c8f1dac2a8cfb996060952f85b
ham0612 <hyemangi@naver.com>
2022-09-04 15:50:26 +0900

    Merge branch 'main' of https://github.com/ham0612ham/ddCafe.git into main

--- ddCafe/src/main/java/ddCafe_Customer/App.java
+++ ddCafe/src/main/java/ddCafe_Customer/App.java
@@ -60,35 +60,6 @@ public class App {
 				if(log==1) {
 					while(true) {
 					try {
-						System.out.println("                                                 /===-_---~~~~~~~~~------____");
-						System.out.println("                                                |===-~___                _,-'");
-						System.out.println("                 -==\\\\                         `//~\\\\   ~~~~`---.___.-~~");
-						System.out.println("             ______-==|                         | |  \\\\           _-~`");
-						System.out.println("       __--~~~  ,-/-==\\\\                        | |   `\\        ,'");
-						System.out.println("    _-~       /'    |  \\\\                      / /      \\      /");
-						System.out.println("  .'        /       |   \\\\                   /' /        \\   /'");
-						System.out.println(" /  ____  /         |    \\`\\.__/-~~ ~ \\ _ _/'  /          \\/'");
-						System.out.println("/-'~    ~~~~~---__  |     ~-/~         ( )   /'        _--~`");
-						System.out.println("                  \\_|      /        _)   ;  ),   __--~~");
-						System.out.println("                    '~~--_/      _-~/-  / \\   '-~ \\");
-						System.out.println("                   {\\__--_/}    / \\\\_&gt;- )&lt;__\\      \\");
-						System.out.println("                   /'   (_/  _-~  | |__&gt;--&lt;__|      |");
-						System.out.println("                  |0  0 _/) )-~     | |__&gt;--&lt;__|     |");
-						System.out.println("                  / /~ ,_/       / /__&gt;---&lt;__/      |");
-						System.out.println("                 o o _//        /-~_&gt;---&lt;__-~      /");
-						System.out.println("                 (^(~          /~_&gt;---&lt;__-      _-~");
-						System.out.println("                ,/|           /__&gt;--&lt;__/     _-~");
-						System.out.println("             ,//('(          |__&gt;--&lt;__|     /                  .----_");
-						System.out.println("            (   '))          |__&gt;--&lt;__|    |                 /' _---_~\\");
-						System.out.println("         `-))  ) (           |__&gt;--&lt;__|    |               /'  /     ~\\`\\");
-						System.out.println("        ,[  어서  ]((             \\__&gt;--&lt;__\\    \\            /'  //        ||");
-						System.out.println("      ,( '    )( )            ~-__&gt;--&lt;_~-_  ~--____---~' _/'/        /'");
-						System.out.println("    `~[  오세용  ]/|                 ~-_~&gt;--&lt;_/-__       __-~ _/");
-						System.out.println("  ._-~//      )) `                    ~~-'_/_/ /~~~~~~~__--~");
-						System.out.println("   ;'( ')/ ,)(                              ~~~~~~~~~~");
-						System.out.println("  ' ') '( (/");
-						System.out.println("    '   '  `");
-
 							int ch;
 							do {
 								System.out.print("\n1.메뉴관리 2.매출관리 3.재고관리 4.회원관리 5.돌아가기 => ");
