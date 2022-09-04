ee9b3253d900a38d199f7c0176b6909d9f1e33fe
choi <choi@DESKTOP-5O4R01C>
2022-09-04 10:45:55 +0900

    최수빈 수정

--- ddCafe/src/main/java/ddCafe_Manage/MemberUI.java
+++ ddCafe/src/main/java/ddCafe_Manage/MemberUI.java
@@ -11,7 +11,6 @@ public class MemberUI {
 	public void menu() {
 
 		int ch;
-		
 
 		while (true) {
 			System.out.println("\n✦ 회원 메뉴 ✦");
@@ -70,8 +69,8 @@ public class MemberUI {
 				return;
 			}
 
-			System.out.println();
 			System.out.println("회원번호 / 회원이름 / 전화번호 / 회원등록일");
+			System.out.println("-----------------------------------------------------------------------");
 
 			for (MemberDTO dto : list) {
 				System.out.print(dto.getMemberNum() + " / ");
@@ -116,8 +115,8 @@ public class MemberUI {
 				return;
 			}
 
-			System.out.println();
 			System.out.println("회원번호 / 회원이름 / 전화번호 / 회원등록일");
+			System.out.println("-----------------------------------------------------------------------");
 
 			System.out.print(dto.getMemberNum() + " / ");
 			System.out.print(dto.getName() + " / ");
@@ -135,8 +134,8 @@ public class MemberUI {
 	public void listMember() {
 		System.out.println("\n✦ 회원 리스트 ︎✦");
 
-		System.out.println();
 		System.out.println("회원번호 / 회원이름 / 전화번호 / 회원등록일");
+		System.out.println("-----------------------------------------------------------------------");
 
 		List<MemberDTO> list = dao.listMember();
 
